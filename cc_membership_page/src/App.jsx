d08703efa36d5ab2cdc0ee8e7d0a117738079b81
Arturyux <artur.burlakin@gmail.com>
2024-11-18 15:25:51 +0100

    Setting up DOM

--- cc_membership_page/src/App.jsx
+++ cc_membership_page/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Main from './main';
+import LoginPanel from './LoginPanel';
+import Registration from './Registration';
+import Submit from './Submit';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* for the future to combine to the main web page */}
+        {/* <Route path="/" element={<main />} /> */}
+        <Route path="/" element={<LoginPanel />} />
+        <Route path="/registartion" element={<Registration />} />
+        <Route path="/submit" element={<Submit />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
