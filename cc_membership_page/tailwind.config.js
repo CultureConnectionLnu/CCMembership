1bd1518188245dfe6ba59973d133fb52ce53accc
Arturyux <artur.burlakin@gmail.com>
2024-11-18 15:49:01 +0100

    Login/Registaration front end done, tailwind fixed

--- cc_membership_page/tailwind.config.js
+++ cc_membership_page/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "7px 7px 4px 1px rgba(0, 0, 0, 1);"
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /^bg-/,
+      variants: ['hover', 'focus'],
+    },
+  ],
 }
-
