febd294c534ea6a7124424d529079a810b0b0b95
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-11-22 16:08:21 +0100

    clerk integration

--- cc_membership_page/src/main.jsx
+++ cc_membership_page/src/main.jsx
@@ -2,6 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Clerk } from "@clerk/clerk-js";
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const clerk = new Clerk(clerkPubKey);
+
+await clerk.load();
+
+if (clerk.user) {
+  document.getElementById("app").innerHTML = `
+    <div id="user-button"></div>
+  `;
+
+  const userButtonDiv =
+    document.getElementById("user-button");
+
+  clerk.mountUserButton(userButtonDiv);
+} else {
+  document.getElementById("app").innerHTML = `
+    <div id="sign-in"></div>
+  `;
+
+  const signInDiv =
+    document.getElementById("sign-in");
+
+  clerk.mountSignIn(signInDiv);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
